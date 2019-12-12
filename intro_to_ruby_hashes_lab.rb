8bf4374c991354e7c571cd7702a92bff3fb4a564
Dan Foley <dan@dan-foley.com>
2019-12-12 20:38:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -68,13 +68,13 @@ def monopoly_with_fourth_tier
 	        :mortgage_value => "$100"
 	      },
 	      :b_and_o_railroad => {
-	        :mortgage_value => "$25"
+	        :mortgage_value => "$400"
 	      },
 	      :pennsylvania_railroad => {
-	        :mortgage_value => "$25"
+	        :mortgage_value => "$200"
 	      },
 	      :shortline_railroad => {
-	        :mortgage_value => "$25"
+	        :mortgage_value => "$800"
 	      }
 	    }
 	  }
